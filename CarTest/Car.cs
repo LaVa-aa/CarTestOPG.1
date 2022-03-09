217b732f2a3a8d57272589b4717599536179beb8
Lava Maamo <lama26830@edu.ucl.dk>
2022-03-09 09:44:14 +0100

    Add project files.

---
+++ CarTest/Car.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace CarTest
+{
+    public class Car
+    {
+        private int id;
+        private string model;
+        private double price;
+        private string licenseplatee;
+
+        public int Id { get; set; }
+
+        public string Model
+        {
+            get => model;
+            set
+            {
+                if (value == null) throw new ArgumentNullException("Model", "Null");
+                if (value.Length < 4) throw new ArgumentOutOfRangeException("Model length must be at least have 3 charaters");
+                model = value;
+            }
+        }
+
+        public double Price
+        {
+            get => price;
+            set
+            {
+                if (value <= 0) throw new ArgumentOutOfRangeException("price", "Price must be higher than 0");
+                price = value;
+
+            }
+        }
+        public string LicensePlate
+        {
+            get => licenseplatee;
+            set
+            {
+                if (value.Length >= 2 && value.Length <= 7) licenseplatee = value;
+                else throw new ArgumentOutOfRangeException("licenseplate", value, "Wrong licenseplate");
+            }
+        }
+
+        public Car()
+        {
+            
+        }
+
+        public Car(int id, string model,double price,string licenseplatee)
+        {
+            Id = id;
+            Model = model;
+            Price = price;
+            LicensePlate = licenseplatee;
+        }
+
+
+        public override string ToString()
+        {
+            return " ID " + id + " Model " + Model + " Price " + Price + " LicensePlate " + LicensePlate;
+        }
+
+
+    }
+}
---
+++ CarTestTests/CarTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarTest.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        private Car car;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            car = new Car(2, "firee", 45678, "wkwo2");
+        }
+        [TestMethod()]
+        public void ConstructorTest()
+        {
+            Car car1 = new Car(2, "firee", 45678, "wkwo2");
+
+            Assert.AreEqual(2,car1.Id);
+            Assert.AreEqual("firee",car1.Model);
+            Assert.AreEqual(45678, car1.Price);
+            Assert.AreEqual("wkwo2", car1.LicensePlate);
+
+        }
+
+        [TestMethod()]
+        public void PriceTest()
+        {
+            Assert.AreEqual(45678, car.Price);
+            Assert.ThrowsException<ArgumentOutOfRangeException>((() => car.Price = 0));
+        }
+
+        [TestMethod()]
+        public void ModelTest()
+        {
+            Assert.AreEqual("firee", car.Model);
+            Assert.ThrowsException<ArgumentNullException>((() => car.Model = null));
+            Assert.ThrowsException<ArgumentOutOfRangeException>((() => car.Model = "hi"));
+        }
+
+        [TestMethod()]
+        public void LicensePlateTest()
+        {
+            Assert.AreEqual("wkwo2",car.LicensePlate);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => car.LicensePlate = "h");
+        }
+
+
+
+    }
+}
